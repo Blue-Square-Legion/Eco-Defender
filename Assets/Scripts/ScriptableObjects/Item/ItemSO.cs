0f9acbbfac1775481296bb4b8fe437b1009735ba
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-04-09 13:36:51 -0700

    Added basic Demo Scene + Basic Scriptable Object for Item

---
+++ Assets/Scripts/ScriptableObjects/Item/ItemSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ItemType
+{
+    Resource,
+    Ammo,    
+    Weapon
+}
+
+[CreateAssetMenu(fileName = "Eco", menuName = "Eco_Item/Item")]
+public class ItemSO : ScriptableObject
+{
+    public ItemType Type;
+}
