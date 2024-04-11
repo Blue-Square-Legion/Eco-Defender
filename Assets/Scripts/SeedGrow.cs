be803c03430e241ed9aa44237b6eb5f1ec388e11
Brandon Jeon <62401369+BJeon04@users.noreply.github.com>
2024-04-10 22:34:07 -0400

    Seed Grow
    Worked on the Seed Grow a bit. I coded two scripts and made one prefab. The sphere has a script called Seed Collision that when it collides with anything it will disappear. A flower will then appear in place of the seed object.

--- Assets/Scripts/SeedGrow.cs
+++ Assets/Scripts/SeedGrow.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class SeedGrow : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Camera camera;
+    //public Camera camera;
+    public float time = 0f;
+    float finalScale = 2.0f;
     void FixedStart()
     {
-        transform.LookAt(camera.transform);
+        //transform.LookAt(camera.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Time.deltaTime 
+        if(time < 3)
+         {
+            //transform.localScale = new Vector3(1.5f,1.5f,1.5f);
+            //transform.
+            var newScale = Mathf.Lerp(1.0f, finalScale, time);
+            transform.localScale = new Vector3(newScale, 1, newScale);
+            time += Time.deltaTime;
+          }
         
     }
 }
