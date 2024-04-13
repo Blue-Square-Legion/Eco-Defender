c679f56a83167b46000966320a74cae6f953f83c
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-04-13 16:18:23 -0700

    Audio Helper Scripts for Player Controller / Shoot

---
+++ Assets/Scripts/Audio/Shoot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AudioScript
+{
+    public class Shoot : MonoBehaviour
+    {
+        public void OnShoot()
+        {
+            print("Shoot");
+        }
+    }
+}
+
