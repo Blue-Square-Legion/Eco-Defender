d8ba32ad6b0cb928cb8f964e09523e3c1d6aa875
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-04-14 13:37:48 -0700

    Added Editor override for Toggle grab

--- Assets/Scripts/XRSimAutoEnable.cs
+++ Assets/Scripts/XRSimAutoEnable.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRSimAutoEnable : MonoBehaviour
 {
     [SerializeField, Tooltip("In editor, will always deacitave the XR Sim, in build, will always activate the XR Sim")] private bool _overrideActive;
     [SerializeField] GameObject _xrSim;
+
+    [SerializeField] private XRBaseControllerInteractor.InputTriggerType _editorOverride = XRBaseControllerInteractor.InputTriggerType.Toggle;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class XRSimAutoEnable : MonoBehaviour
         {
 #if UNITY_EDITOR
         _xrSim.SetActive(!_overrideActive);
+
+        OverrideSelect(_editorOverride);
+
 #else
         _xrSim.SetActive(_overrideActive);
 #endif
@@ -20,6 +26,18 @@ public class XRSimAutoEnable : MonoBehaviour
         else 
         {
             Debug.LogWarning("No XR Sim assigned!");
+        }
+    }
+
+
+    private void OverrideSelect(XRBaseControllerInteractor.InputTriggerType value)
+    {
+        //XRBaseControllerInteractor are inactive at start? 
+        XRBaseControllerInteractor[] list = gameObject.GetComponentsInChildren<XRBaseControllerInteractor>(true);
+        print(list.Length);
+        foreach (XRBaseControllerInteractor item in list)
+        {
+            item.selectActionTrigger = value;
         }
     }
 }
