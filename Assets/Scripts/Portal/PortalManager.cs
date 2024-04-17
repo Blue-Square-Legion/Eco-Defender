83a5ef3697d4bd9daa535cbcec571a0e647e27c6
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-04-16 23:00:32 -0700

    Added Throttle for Open/Close.
    Removed Large plant prefab (Was throwing error)

--- Assets/Scripts/Portal/PortalManager.cs
+++ Assets/Scripts/Portal/PortalManager.cs
@@ -5,29 +5,54 @@ using System.Threading.Tasks;
 
 public class PortalManager : MonoBehaviour
 {
-    [SerializeField] private AnimatorController _DoorAnim;
+    [SerializeField] private AnimatorController _DoorAnimator;
   
     private SceneManagerSO _currentScene = null;
 
+    private bool _isBlocked = false;    //throtle open/close
+
     public async void Open(SceneManagerSO manager)
+    {
+        if (_isBlocked)
+        {
+            return;
+        }
+
+        _isBlocked = true;
+        await AsyncOpen(manager);
+        _isBlocked = false;
+    }
+
+    public async void Close()
+    {
+        if (_isBlocked)
+        {
+            return;
+        }
+
+        _isBlocked = true;
+        await AsyncClose();
+        _isBlocked = false;
+    }
+
+
+    //Async non-void to allow await
+    private async Task<bool> AsyncOpen(SceneManagerSO manager)
     {
         if (_currentScene)
         {
-            Debug.LogWarning("Scene is Already Loaded");
+            Debug.LogWarning("Scene is Already Loaded. Closing Previous then Opening.");
             await AsyncClose();
         }
 
         await manager.AsyncLoad();
         _currentScene = manager;
-        _DoorAnim.Open();
-    }
+        _DoorAnimator.Open();
 
-    public async void Close()
-    {
-        await AsyncClose();
+        await Task.Delay((int)(_DoorAnimator.ClipLength * 1000));
+        return true;
     }
-
-    //Async non-void to allow await 
+    
     private async Task<bool> AsyncClose()
     {
         if(_currentScene == null)
@@ -35,9 +60,9 @@ public class PortalManager : MonoBehaviour
             return false;
         }
 
-        _DoorAnim.Close();
+        _DoorAnimator.Close();
 
-        await Task.Delay((int)(_DoorAnim.ClipLength * 1000));
+        await Task.Delay((int)(_DoorAnimator.ClipLength * 1000));
         await _currentScene.AsyncUnload();
 
         _currentScene = null;
