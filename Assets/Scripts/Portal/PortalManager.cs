b074f5472c36a584bc88fda68df8a5589daadb62
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-04-16 21:08:15 -0700

    Added PortalManager & updated SceneManagerSO to be Async

---
+++ Assets/Scripts/Portal/PortalManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class PortalManager : MonoBehaviour
+{
+    [SerializeField] private AnimatorController _DoorAnim;
+  
+    private SceneManagerSO _currentScene = null;
+
+    public async void Open(SceneManagerSO manager)
+    {
+        if (_currentScene)
+        {
+            Debug.LogWarning("Scene is Already Loaded");
+            await AsyncClose();
+        }
+
+        await manager.AsyncLoad();
+        _currentScene = manager;
+        _DoorAnim.Open();
+    }
+
+    public async void Close()
+    {
+        await AsyncClose();
+    }
+
+    //Async non-void to allow await 
+    private async Task<bool> AsyncClose()
+    {
+        _DoorAnim.Close();
+
+        await Task.Delay((int)(_DoorAnim.ClipLength * 1000));
+        await _currentScene.AsyncUnload();
+
+        _currentScene = null;
+        return true;
+    }
+
+
+}
