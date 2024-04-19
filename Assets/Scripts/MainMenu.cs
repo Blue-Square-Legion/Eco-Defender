990f6033b4485f0962e5cbc06b664d589106523a
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-04-19 15:05:22 -0700

    test

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,8 @@
-using DevTool;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] Dev_SceneManger dev_SceneManger;
+
     [SerializeField] GameObject startButton;
     [SerializeField] GameObject optionsButton;
 
@@ -14,14 +11,14 @@ public class MainMenu : MonoBehaviour
     {
         startButton.SetActive(true);
         // Make sure the portal animation is not player and in close mode for when a player comes back from level
-        dev_SceneManger.UnLoad();
+        //dev_SceneManger.UnLoad();
     }
 
     public void StartLevel()
     {
         startButton.SetActive(false);
         // Play portal animation
-        dev_SceneManger.Load();
+        //dev_SceneManger.Load();
     }
 
 
