6bd1e3ab35714a96299ec4e6dcf4351a6bee06e3
Samuel325 <ayenisamuel87@gmail.com>
2024-04-23 16:47:08 -0400

    Update PlayerRayInteractor.cs

--- Assets/Scripts/Gameplay/PlayerRayInteractor.cs
+++ Assets/Scripts/Gameplay/PlayerRayInteractor.cs
@@ -8,6 +8,7 @@ public class PlayerRayInteractor : XRRayInteractor
 {
     [Header("Custom Data")]
     public UnityEvent OnGunHeld;
+    public UnityEvent OnGunReleased;
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
@@ -16,6 +17,18 @@ public class PlayerRayInteractor : XRRayInteractor
         if(args.interactableObject.transform.gameObject.GetComponent<Gun>())
         {
             OnGunHeld.Invoke();
+            Debug.Log("Gun held");
+        }
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+
+        if (args.interactableObject.transform.gameObject.GetComponent<Gun>())
+        {
+            OnGunReleased.Invoke();
+            Debug.Log("Gun released");
         }
     }
 }
