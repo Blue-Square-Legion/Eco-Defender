05dd4e74196e63281a94961a8b58b9783bc38ae7
Samuel325 <ayenisamuel87@gmail.com>
2024-05-25 18:13:20 -0400

    Crafting Items Pickup On Walking Over
    Items can now be picked up when you walk over them. Wasn't sure weather to use "ItemTag" or "ItemPickUp" for the collector component since they both have the same code but "ItemPickUp" has more code involved with it than "ItemTag". UI popup shows the collected message briefly before disappearing or getting updated again.

--- Assets/Scripts/Collectable/CollectorComponent.cs
+++ Assets/Scripts/Collectable/CollectorComponent.cs
@@ -1,26 +1,67 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-using EventSO;
+using UnityEngine.Events;
 
 public class CollectorComponent : MonoBehaviour
 {
     [SerializeField] private Inventory _invRef;
+    [SerializeField] private GameObject popupMessage;
+    [SerializeField] private float messageDuration = 0.3f;
+
+    private LinkedList<string> _popupMessages;
 
     public Inventory InvRef
     {
         get { return _invRef; }
     }
 
+    private void Start()
+    {
+        _popupMessages = new LinkedList<string>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<ItemTag>())
+        if(other.GetComponent<ItemPickUp>())
         {
-            _invRef.AddToInventory(other.gameObject.GetComponent<ItemTag>().Tag);
+            _invRef.AddToInventory(other.gameObject.GetComponent<ItemPickUp>().ItemSO);
+
+            if (_invRef.Inv.ContainsKey(other.GetComponent<ItemPickUp>().ItemSO))
+            {
+                _popupMessages.AddLast("You got " + _invRef.Inv[other.GetComponent<ItemPickUp>().ItemSO] + " " + other.GetComponent<ItemPickUp>().ItemSO.name + "s.");
+            }
+            else
+            {
+                _popupMessages.AddLast("You got " + (other.GetComponent<ItemPickUp>().Quantity > 1 ? other.GetComponent<ItemPickUp>().Quantity + other.GetComponent<ItemPickUp>().ItemSO.name + "s." : "one " + other.GetComponent<ItemPickUp>().ItemSO.name));
+            }
             
+            StartCoroutine(ShowMessage());
+
             Destroy(other.gameObject);
+        }
+    }
+
+    IEnumerator ShowMessage()
+    {
+        TextMeshProUGUI pM = popupMessage.GetComponent<TextMeshProUGUI>();
+
+        if (_popupMessages.First != null)
+        {
+            pM.SetText(_popupMessages.First.Value);
+        }
+
+        yield return new WaitForSeconds(pM.text.Split(' ').Length * messageDuration);
+
+        if (_popupMessages.First != null)
+        {
+            _popupMessages.RemoveFirst();
+            StartCoroutine(ShowMessage());
+        } else
+        {
+            pM.SetText("");
         }
     }
 }
