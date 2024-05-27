0271f9993d831a1d937bd2608e6d68351f316c4d
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-05-27 14:55:14 -0700

    Removed Destroy self on contact

--- Assets/Scripts/Gameplay/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemy.cs
@@ -80,7 +80,7 @@ public class Enemy : MonoBehaviour, IDamageable
         {
             print($"Enemy Trigger: {other.name}");
             NotifyDamage(other);
-            Destroy(gameObject, 1f);
+            //Destroy(gameObject, 1f);  //Originally destroyed self on contact to prevent pinning player.
         }
     }
 
@@ -128,7 +128,7 @@ public class Enemy : MonoBehaviour, IDamageable
             else
                 OnMoveEnd.Invoke();
         }
-        
+
         yield return new WaitForSeconds(navTickTime);
         StartCoroutine(UpdateDestination());
     }
