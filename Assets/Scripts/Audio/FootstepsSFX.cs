88cb5d70a848923207f119358408f74491d1b868
seribeengton <seribeengton@gmail.com>
2024-06-08 18:30:05 -0400

    Player Footstep
    -Added FootstepsSFX.cs to Audio scripts folder
    -Added StarterAssets to the script assembly definition
    -Added FootstesSFX to the player capsul prefab. Currently uses slime walk sfx for testing purposes

---
+++ Assets/Scripts/Audio/FootstepsSFX.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using UnityEngine.Events;
+
+public class FootstepsSFX : MonoBehaviour
+{
+
+    [SerializeField]private FirstPersonController FPS;
+    [SerializeField]private CharacterController playerController;
+
+    public string OnMoveStartEvent = "Play_Slime_Footstep_Loop";
+    public string OnMoveEndEvent = "Stop_Slime_Footstep_Loop";
+    
+    private bool playerWasMoving = false;
+
+    public UnityEvent PlayFootsteps;
+    public UnityEvent StopPlayingFootsteps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (FPS == null) 
+        {
+            FPS = GetComponent<FirstPersonController>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerFootSteps();
+    }
+
+    private void PlayerFootSteps() 
+    {
+        bool isCurMoving = isPlayerMoving();
+       
+        if (playerWasMoving != isCurMoving)
+        {
+            playerWasMoving = isCurMoving;
+            if (isCurMoving)
+            {
+                OnPlayerStart();
+            }
+            else
+            {
+                OnPlayerStop();
+            }
+        }
+    }
+
+    private bool isPlayerMoving()
+    {
+        return FPS.Grounded && playerController.velocity != Vector3.zero;
+    }
+
+    private void OnPlayerStart() 
+    {
+        AkSoundEngine.PostEvent(OnMoveStartEvent, gameObject);
+        print("PlayerMoveStar");
+        PlayFootsteps?.Invoke();
+    }
+
+    private void OnPlayerStop() 
+    {
+        AkSoundEngine.PostEvent(OnMoveEndEvent, gameObject);
+        print("PlayerMoEnd");
+        StopPlayingFootsteps?.Invoke();
+    }
+}
