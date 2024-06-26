4f4725a6b7fce8a9abd54bff068274319f193a06
XanferS <Funntmatthew@hotmail.com>
2024-06-26 17:33:55 -0400

    User able to holster gun (Primary object) and swap between two
    The player guns in the lab scene are able to be picked up and held onto by the player, currently their are a couple issues
    when both guns are picked up and swapped, they will no longer fire.
    there seems to be an issue with unequipping the second gun after swapping.

--- Assets/Scripts/Player/FPSPlayerInteractionController.cs
+++ Assets/Scripts/Player/FPSPlayerInteractionController.cs
@@ -13,6 +13,8 @@ public class FPSPlayerInteractionController : MonoBehaviour, IPlayerEquip
     public UnityEvent OnItemPickup;
 
     public GameObject PrimaryObject;
+    public GameObject holsteredObject;
+    private GameObject transferObject;
     [SerializeField] private Transform handSlot;
     [SerializeField] private LayerMask BlockMask, UsableMask;
     [SerializeField] private float radius = 0.2f, distance = 10f;
@@ -55,6 +57,49 @@ public class FPSPlayerInteractionController : MonoBehaviour, IPlayerEquip
             return;
         }
     }
+    void Update()
+    {
+        //testing code
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Debug.Log("keypad1 was pressed");
+            if (PrimaryObject != null && holsteredObject == null)
+            {
+                Debug.Log("primary object was detected");
+                holsteredObject = PrimaryObject;
+                PrimaryObject = null;
+                Debug.Log("primary gun is now holstered");
+                Holster(holsteredObject);
+            }
+            else if (holsteredObject != null && PrimaryObject == null)
+            {
+                Debug.Log("primary object was not detected");
+                unHolster(holsteredObject);
+                PrimaryObject = holsteredObject;
+                holsteredObject = null;
+                Debug.Log("primary gun is now unholstered");
+            }
+            else if(PrimaryObject != null && holsteredObject != null)
+            {
+                Debug.Log("Both object slots are full");
+                transferObject = PrimaryObject;
+                PrimaryObject = null;
+                PrimaryObject = holsteredObject;
+                holsteredObject = transferObject;
+                transferObject = null;
+                swapGun(PrimaryObject, holsteredObject);
+            }
+            else
+            {
+                Debug.Log("primary object is not null");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            UnEquip(PrimaryObject);
+        }
+    }
+
 
 
 #if UNITY_EDITOR
@@ -93,8 +138,21 @@ public class FPSPlayerInteractionController : MonoBehaviour, IPlayerEquip
 
     public void OnUse()
     {
-        if (enabled) { usable?.Use(); }
-
+        //code from seribeengton
+        //if (enabled) { usable?.Use(); }
+        if (PrimaryObject != null)
+        {
+            Debug.Log("primary object is detected for firing");
+            if (PrimaryObject.activeSelf)
+            {
+                Debug.Log("Gun has fired");
+                usable?.Use();
+            }
+        }
+        else
+        {
+            Debug.Log("Error has occurred with detected if the object isnt null");
+        }
     }
 
     public void OnAltUse()
@@ -186,4 +244,52 @@ public class FPSPlayerInteractionController : MonoBehaviour, IPlayerEquip
 
         SetObject(null);
     }
+
+    /* <summary>
+     * Allows the currently held gun to be set inactive while following the player and being unrendered, and will allow the second gun to be rendered in place and interactable.
+     * 1) create a function to be called that swaps between two guns when the player has two guns
+     * 2) create a function that can just store one gun
+     * 3) create a holster value that can be filled when picking up a second gun when the first gun has not been stored
+     */
+    private bool holsterBool = false;
+    public void Holster(GameObject obj)
+        //set gun as inactive
+    {
+        obj.SetActive (false);
+        holsterBool = true;
+        //set a boolean to say that the holster is "full"
+    }
+    public void unHolster(GameObject obj)
+        //set gun as active
+    {
+        obj.SetActive (true);
+        holsterBool = false;
+        //set a boolean to say that the holster is "empty"
+    }
+    public void swapGun(GameObject obj1, GameObject obj2)
+        //sets gun 1 to inactive and gun 2 as active, overwrites holster bool to remain full.
+    {
+        print($"Swap: {obj1} with {obj2}");
+        if (holsterBool == true)
+        {
+            Holster(obj1);
+            unHolster(obj2);
+            holsterBool = true;
+            print($"Weapon: {obj1} is holstered and {obj2} is unholstered");
+        }
+        else
+        {
+            print($"The user does not have a second gun");
+        }
+    }
+    public void newGun(GameObject obj1, GameObject obj2)
+        //sets current gun to inactive and the picked up gun as active
+        // not currently in use, needs to be used with the equip function.
+    {
+        print($"Store: {obj1} and equip {obj2}");
+        Holster(obj1);
+        Equip(obj2);
+        holsterBool = true;
+    }
+
 }
